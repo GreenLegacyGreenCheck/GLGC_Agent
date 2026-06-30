4ae8e2329382b0e99f6bfef9cc56b2e7a187bd50
chltndus0401 <ally5336@duksung.ac.kr>
2026-06-30 19:09:05 +0900

    fix port num

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -44,4 +44,5 @@ async def get_crawled_data(refresh: bool = False):
     return {"status": "success", "total_count": len(valid_data_list), "data": valid_data_list}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    port = int(os.environ.get("PORT", 8001))
+    uvicorn.run(app, host="0.0.0.0", port=port)
